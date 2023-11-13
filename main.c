f5368887c2f7ad8f40749e256bc78c260e83d32c
Tallysson Yuri <115179564+tallyssonyuri@users.noreply.github.com>
2023-11-13 02:54:55 +0000

    O menu foi refeito adicionando as novas funcionalidades e chamando as funções responsáveis

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 int main() {
   printf("\nBem vindo ao app --FACA HOJE!--\n\n");
-  
+
   printf("Desenvolvemos esse aplicativo com o objetivo de te ajudar com suas metas do dia-a-dia, definir a sua prioridade e conseguir organizar melhor a sua vida\n\n");
 
   printf("Aqui você poderá criar tarefas, lista-las, definir niveis de prioridade e deleta-las quando quiser!\n\n");
@@ -13,7 +13,7 @@ int main() {
   printf("Este programa possui capacidade de armazenar até 100 tarefas, esperamos que você goste :)\n\n\n");
 
   printf("Vamos comecar? Digite a opcao desejada abaixo:\n\n");
-  
+
   int cod; /* Variável para armazenar códigos de retorno */
   char arquivo[100] = "tarefas"; /* Nome do arquivo para armazenar as tarefas */
   ListaDeTarefas lt; /* Estrutura para armazenar a lista de tarefas */
@@ -32,18 +32,32 @@ int main() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF); /* Limpa o buffer de entrada, pois será preciso para não atrapalhar na execução das demais funções no programa */
 
-    if(opcao == 4){ /* Esta opção executará o fim do loop */
-/* Abaixo será a escolha das 3 funções do programa */
-    } else if(opcao == 1){
-      cadastrarTarefas(&lt);
-    } else if(opcao == 2){
-      deletarTarefas(&lt);
-    } else if(opcao == 3){
-      listarTarefas(lt);
-    } else {
-      printf("Opcao invalida!\n\n");
+    switch(opcao) { /*Este Switch case será responsável por chamar as funções principais do programa e executa-las*/
+      case 1:
+        cadastrarTarefas(&lt);
+        break;
+      case 2:
+        deletarTarefas(&lt);
+        break;
+      case 3:
+        listarTarefas(lt);
+        break;
+      case 4:
+        alterarTarefas(&lt);
+        break;
+      case 5:
+        filtrarTarefas(lt);
+        break;
+      case 6:
+        exportarTarefas(lt);
+        break;
+      case 7:
+        printf("Saindo...\n\n");
+        break;
+      default:
+        printf("Opcao invalida!\n\n");
     }
-  } while (opcao != 4);
+  } while (opcao != 7);
 
   cod = salvarTarefas(&lt, arquivo); /* Salva as tarefas no arquivo */
   if (cod != 0){ /* Se houve um problema ao salvar, informará ao usuario */
